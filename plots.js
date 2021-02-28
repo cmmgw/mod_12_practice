8e46348c360046b5c032dd87e5d519d009955b61
cmmgw <cecily.martin@gmail.com>
2021-02-27 19:19:54 -0500

    add files

---
+++ plots.js
@@ -0,0 +1,9 @@
+// var cityGrowths = [
+//     {
+//       "Rank": 1,
+//       "City": "San Antonio ",
+//       "State": "Texas",
+//       "Increase_from_2016": "39208",
+//       "population": "1511946"
+//     },
+console.log(cityGrowths);
